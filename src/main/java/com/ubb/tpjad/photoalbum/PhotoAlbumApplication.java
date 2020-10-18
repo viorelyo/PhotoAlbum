2413fee782f5f1c3bbb7e4eaced327e852ddfe2e
cherryDevBomb <turcan.olguta@gmail.com>
2020-10-18 18:15:35 +0300

    add structure

--- src/main/java/com/ubb/tpjad/photoalbum/PhotoAlbumApplication.java
+++ src/main/java/com/ubb/tpjad/photoalbum/PhotoAlbumApplication.java
@@ -3,8 +3,9 @@ package com.ubb.tpjad.photoalbum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @Slf4j
 public class PhotoAlbumApplication {
 
