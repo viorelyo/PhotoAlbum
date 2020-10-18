cded2d17d79c411285f62962915154e259c2fa38
cherryDevBomb <turcan.olguta@gmail.com>
2020-10-18 14:04:51 +0300

    add logging

--- src/main/java/com/ubb/tpjad/photoalbum/PhotoAlbumApplication.java
+++ src/main/java/com/ubb/tpjad/photoalbum/PhotoAlbumApplication.java
@@ -1,13 +1,15 @@
 package com.ubb.tpjad.photoalbum;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PhotoAlbumApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoAlbumApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAlbumApplication.class, args);
+        log.info("hello");
+    }
 }
