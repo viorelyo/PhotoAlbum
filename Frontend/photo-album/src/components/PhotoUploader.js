33e48d26bda6a0af671d20f51941be36452e4e36
cherryDevBomb <turcan.olguta@gmail.com>
2020-12-20 19:55:27 +0200

    add UI error messages for failed upload

--- Frontend/photo-album/src/components/PhotoUploader.js
+++ Frontend/photo-album/src/components/PhotoUploader.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Dropzone from "react-dropzone";
-import { List, Icon, Segment } from "semantic-ui-react";
+import {Icon, Message, MessageContent, Segment} from "semantic-ui-react";
 
-import { uploadPhoto } from "../api/photosApi";
+import {uploadPhoto} from "../api/photosApi";
 
 class PhotoUploader extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       files: [],
+      error: false,
+      errorMessage: "",
     };
 
     this.fileInputCallback = this.fileInputCallback.bind(this);
+    this.handleDismissError = this.handleDismissError.bind(this);
   }
 
   fileInputCallback(acceptedFiles) {
@@ -21,29 +24,50 @@ class PhotoUploader extends React.PureComponent {
       uploadPhoto(file, albumId).then((status) => {
         //TODO can be shown on page
         if (status === 200) {
-            this.setState({ files: [...this.state.files, file] });
+          this.setState({files: [...this.state.files, file]});
+        } else {
+          this.setState({error: true, errorMessage: "Please try again"});
+          setTimeout(() => {
+            this.setState({error: false, errorMessage: ""})
+          }, 5000)
         }
+      }).catch(e => {
+        this.setState({error: true, errorMessage: "File size is too big"})
+        setTimeout(() => {
+          this.setState({error: false, errorMessage: ""})
+        }, 5000)
       });
     });
   }
 
+  handleDismissError() {
+    this.setState({error: false, errorMessage: ""})
+  }
+
   render() {
     return (
-      <Dropzone onDrop={this.fileInputCallback}>
-        {({ getRootProps, getInputProps }) => (
-          <Segment
-            inverted
-            tertiary
-            color="black"
-            textAlign="center"
-            {...getRootProps()}
-          >
-            <input {...getInputProps()} />
-            <Icon name="cloud upload" size="big" />
-            <div>Drag and drop or upload file</div>
-          </Segment>
-        )}
-      </Dropzone>
+      <React.Fragment>
+        <Dropzone onDrop={this.fileInputCallback}>
+          {({getRootProps, getInputProps}) => (
+            <Segment
+              inverted
+              tertiary
+              color="black"
+              textAlign="center"
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              <Icon name="cloud upload" size="big"/>
+              <div>Drag and drop or upload file</div>
+            </Segment>
+          )}
+        </Dropzone>
+        {this.state.error &&
+        <Message negative onDismiss={this.handleDismissError}>
+          <Message.Header>Upload failed</Message.Header>
+          <MessageContent>{this.state.errorMessage}</MessageContent>
+        </Message>}
+      </React.Fragment>
     );
   }
 }
